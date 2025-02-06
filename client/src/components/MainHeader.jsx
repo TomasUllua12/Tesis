555b2e003ebea3c596a365c3ebaa583b0c50d73f
Tomas Ullua <tomasullua@gmail.com>
2025-02-06 09:29:24 -0300

    mainheader cambiado para darkmode

--- client/src/components/MainHeader.jsx
+++ client/src/components/MainHeader.jsx
@@ -1,9 +1,11 @@
 // Client/components/MainHeader.jsx
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import "./MainHeader.css"; // Puedes reutilizar estilos o crear uno nuevo
+import { DarkModeContext } from "../context/DarkModeContext";
 
 export function MainHeader({ title }) {
   const [user, setUser] = useState(null);
+  const { darkMode } = useContext(DarkModeContext);
 
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
@@ -13,12 +15,12 @@ export function MainHeader({ title }) {
   }, []);
 
   return (
-    <div className="general-main-encabezado">
+    <div className={darkMode ? "general-main-encabezado-dark" : "general-main-encabezado"}>
       <h2>{title}</h2>
       <div className="perfil-expmon-header">
         <p>
-          <span className="exp-text">Exp</span>{" "}
-          {user ? user.experience : 0} - <b></b>
+          <span className="exp-text">Exp</span> {user ? user.experience : 0} -{" "}
+          <b></b>
           <span>
             <img src="/public/Coin.gif" alt="Coins" className="gif-span" />
           </span>
