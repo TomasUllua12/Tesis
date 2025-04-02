b97829e90e0959f94439bff0c6627c899e7dfb97
Tomas Ullua <tomasullua@gmail.com>
2025-04-02 20:07:44 -0300

    inicio de App modificado

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,17 @@ function App() {
 
   return (
     <>
-      <Link to="Login"><h2>Login</h2></Link>
+    <div className='app-bg'>
+      <div className='app-todo'>
+        <div className='cuadrado'>
+          <p className='app-p'>Bienvenidos a</p>
+          <img className="app-logo" src="../public/FinverseLogoDark.png" alt="" />
+          <p className='app-p'>Lorem, ipsum dolor sit amet te aut impedit molestiae excepturi?</p>
+          <Link to="Login"><h2>Ingresar</h2></Link>
+        </div>
+      </div>
+      <div className='app-img'></div>
+    </div>
     </>
   )
 }
