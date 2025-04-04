b50b0805ba7beb9512dba58615a04c15dba79289
Tomas Ullua <tomasullua@gmail.com>
2025-04-04 08:47:37 -0300

    App responsive

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,21 +2,18 @@ import './App.css'
 import { Link } from "react-router-dom"
 
 function App() {
-
   return (
-    <>
     <div className='app-bg'>
       <div className='app-todo'>
         <div className='cuadrado'>
           <p className='app-p'>Bienvenidos a</p>
-          <img className="app-logo" src="../public/FinverseLogoDark.png" alt="" />
+          <img className="app-logo" src="../public/FinverseLogoDark.png" alt="Logo Finverse" />
           <p className='app-p'>Cada decisión cuenta. Empieza a tomar el control de tu vida financiera.</p>
           <Link to="Login"><h2>Ingresar</h2></Link>
         </div>
       </div>
       <div className='app-img'></div>
     </div>
-    </>
   )
 }
 
